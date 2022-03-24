667e18807cd020dfae7efec041a80a7f171408c2
Haris Shah <harisshah385@gmail.com>
2022-03-24 12:09:46 +0500

    add form validation javascript

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('hello');
+const form = document.querySelector('.form');
+const nameInput = document.querySelector('.name');
+const emailInput = document.querySelector('.email');
+const passwordInput = document.querySelector('.password');
+const errorMsg = document.querySelector('.errorMsg');
+const formControl = document.querySelector('.form__control');
+
+const formHanlder = (e) => {
+	e.preventDefault();
+
+	const name = nameInput.value;
+	const email = emailInput.value;
+	const password = passwordInput.value;
+
+	if (name === '') {
+		errorHandler('name error', nameInput);
+	}
+	if (email === '') {
+		errorHandler('email error', emailInput);
+	}
+	if (password === '') {
+		errorHandler('password error', passwordInput);
+	}
+};
+
+form.addEventListener('submit', formHanlder);
+
+const errorHandler = (error, input) => {
+	input.classList.add('error');
+	let html = `
+	<span>${error}</span>
+	`;
+
+	formControl.innerHTML += html;
+};
